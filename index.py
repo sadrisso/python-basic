d9ddbe804f7d51cfc9b54fc37949dbf3f86ad284
sadrisso <sadrisso2000@gmail.com>
2024-02-28 17:49:16 +0600

    variable diclire and print

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
+
 print("Hello World");
+
+
+numberOne = 1
+numberTwo = 2
+
+nameOne = 'Shoeb Akter Drisso'
+nameTwo = 'Shakib Al Hasan'
+
+listOne = ['Alu', 'Piyaj', 1, 'Tomato', 1.2]
+
+print(numberOne + numberTwo)
+print(nameOne + nameTwo)
+print(listOne)
