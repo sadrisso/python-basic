b17a49dce63169118c3f7f5c03a56a522e9b63ab
sadrisso <sadrisso2000@gmail.com>
2024-02-29 00:21:04 +0600

    logical operator and if elif

--- index.py
+++ index.py
@@ -43,3 +43,23 @@ else:
     print('Under 18')
 
 # conditional statement end
+
+# logical operator & if elif
+    
+print(10 + 5 == 15 and 10 - 5 == 10) # if one true and one false then he returns false
+print(10 + 5 == 15 and 10 - 5 == 5) # if both are true then he returns true
+
+print(10 > 5 or 10 < 5) # if one logic is true then he return true 
+print(10 > 55 or 100 < 20) # he returns false when both are false
+
+print(not (10 >= 100)) # it reverse 
+
+marks = 89
+if marks >= 90 and marks <= 100:
+    print('You get 2 box of chocolate')
+elif marks >= 80 and marks <= 90:
+    print('You get 1 box of chocolate')
+else:
+    print('You got nothing')
+
+# logical operator and if elif end
