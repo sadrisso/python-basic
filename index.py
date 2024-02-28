8b5dc6e596aa6a275826c8bb6caed7ccee5f3dde
sadrisso <sadrisso2000@gmail.com>
2024-02-28 17:56:04 +0600

    taking user input

--- index.py
+++ index.py
@@ -13,3 +13,8 @@ listOne = ['Alu', 'Piyaj', 1, 'Tomato', 1.2]
 print(numberOne + numberTwo)
 print(nameOne + nameTwo)
 print(listOne)
+
+userName = input("Enter your name: ")
+userAge = input("Enter your age: ")
+print("Hello ",userName, "Your age is ",userAge)
+
