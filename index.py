364d7e0f4690c1f48ad02875b07dd152f46b5b82
sadrisso <sadrisso2000@gmail.com>
2024-02-28 23:28:49 +0600

    conditional statement

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
+# first print python start
 
 print("Hello World");
 
+# first print python end
+
+# variable declire and print start
 
 numberOne = 1
 numberTwo = 2
@@ -14,7 +18,28 @@ print(numberOne + numberTwo)
 print(nameOne + nameTwo)
 print(listOne)
 
+# variable declire and print end
+
+# taking user input start
+
 userName = input("Enter your name: ")
 userAge = input("Enter your age: ")
 print("Hello ",userName, "Your age is ",userAge)
 
+# taking user input end
+
+# conditional statement start
+
+rain = True
+if rain:
+    print('I will not go')
+else:
+    print('I will go')
+
+age = 17
+if age >= 18:
+    print('You are adult')
+else:
+    print('Under 18')
+
+# conditional statement end
