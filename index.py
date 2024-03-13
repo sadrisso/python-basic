db3abe9ed31a0f5df45342284f70b18355173941
sadrisso <sadrisso2000@gmail.com>
2024-03-13 11:03:58 +0600

    problem one calculate length and breadth

--- index.py
+++ index.py
@@ -64,3 +64,11 @@ else:
 
 # logical operator and if elif end
 
+# practice problem one
+    
+a = 10
+
+if(a%2 == 0):
+    print('Even number')
+else:
+    print('Odd number')
---
+++ problem1.py
@@ -0,0 +1,8 @@
+
+length = int(input("Enter the length :"))
+breadth = int(input("Enter the bredth :"))
+
+if(length == breadth):
+    print("This is a square")
+else:
+    print("This is a rectangle")
